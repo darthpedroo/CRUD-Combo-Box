a0cb6114e15c9261a183e788d7631a387175aa70
darthpedroo <pedrovillarino06@gmail.com>
2024-10-04 21:31:29 -0300

    Added seleccionar cliente

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
     items = ['Item 1', 'Item 2', 'Item 3']
+    clientes = ["db.get_clientes()"]
     return render_template('index.html', username='John Doe', items=items)
+
+
+@app.route("/seleccionar-cliente")
+def seleccionar_cliente():
+
+    # clientes = db.get_clientes()
+    clientes = ["db.get_clientes()"]
+
+    return render_template("seleccionar-cliente.html", clientes=clientes)
 
 
 if __name__ == '__main__':
